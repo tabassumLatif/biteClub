d172dbf82959b17d8703d1c540c274ec6424f4ba
TABASSUM\tabassumlatif <tabassum.latif@fourspan.com>
2019-05-18 17:43:36 +0500

    add welcome Scree

--- lib/view/welcome/WelcomePage.dart
+++ lib/view/welcome/WelcomePage.dart
@@ -8,8 +8,8 @@ class WelcomePage extends StatefulWidget {
   _WelcomePageState createState() => _WelcomePageState();
 }
 
-class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStateMixin{
-
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
   void handleTimeout() {
     Navigator.of(context).pushReplacement(new MaterialPageRoute(
         builder: (BuildContext context) => new HomePage()));
@@ -29,8 +29,72 @@ class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStat
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final welcomeText = Text(
+      "Thanks for signing up",
+      style: new TextStyle(
+        fontSize: 22.0,
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
 
+    final subText = Text(
+      "Your request for sign up has been \nrecieved, we will get\n to you shortly",
+      style: new TextStyle(
+        fontSize: 16.0,
+          color: Colors.black54,
+          fontWeight: FontWeight.w500
+      ),
+      textAlign: TextAlign.center,
+    );
+
+    final body = Stack(children: <Widget>[
+      Image.asset(
+        'assets/images/background.png',
+        fit: BoxFit.fill,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+      Center(
+          child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(left: 24.0, right: 24.0),
+              children: <Widget>[
+            new Image.asset(
+              'assets/images/welcome.png',
+              width: 200,
+              height: 159,
+              fit: BoxFit.none,
+            ),
+            new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                  width: 20,
+                  height: 2,
+                  color: Colors.black,
+                ),
+                new Padding(
+                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                ),
+                new Container(
+                  width: 120,
+                  height: 2,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            new Padding(padding: EdgeInsets.fromLTRB(0, 16, 0, 0)),
+            welcomeText,
+                new Padding(padding: EdgeInsets.fromLTRB(0, 8, 0, 0)),
+                subText
+          ]))
+    ]);
+
+    return Scaffold(
+      body: body,
     );
   }
 }
